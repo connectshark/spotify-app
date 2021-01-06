ddfa2d255715ca6507ddfffebff51e84d87a0627
connectshark <bobhus394@gmail.com>
2021-01-06 17:55:07 +0800

    web 不用encodeurl也行

--- src/views/login.vue
+++ src/views/login.vue
@@ -7,9 +7,9 @@
 <script>
 export default {
   setup () {
-    const url = process.env.NODE_ENV === 'production' ? 'https%3A%2F%2Fspotify-app.onrender.com%2Fauthorize' : 'http%3A%2F%2Flocalhost%3A8080%2Fauthorize'
+    const url = process.env.NODE_ENV === 'production' ? 'https://spotify-app.onrender.com/authorize' : 'http://localhost:8080/authorize'
     const type = 'token'
-    const scope = 'user-read-private%20user-read-email'
+    const scope = 'user-read-private&user-read-email'
     return {
       url: `https://accounts.spotify.com/authorize?client_id=${process.env.VUE_APP_CLIENTID}&response_type=${type}&redirect_uri=${url}&scope=${scope}`,
       btnName: '登入spotify'
